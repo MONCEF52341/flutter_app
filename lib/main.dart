01ddcedb770ae3e27210a759c46da20efb9bd440
MONCEF52341 <mostaine.moncef177@gmail.com>
2024-12-02 20:11:03 +0100

    initialCreate

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter_app/firebase_options.dart';
+import 'package:flutter_app/pages/login/login.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'pages/signup/signup.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: Login());
+  }
+}
